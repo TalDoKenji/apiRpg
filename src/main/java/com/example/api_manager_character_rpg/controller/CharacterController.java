f1a4d6aa352ba7f1db02caaad3c0ace21c7b1a35
TalDoKenji <viniciuskenjiuda@gmail.com>
2025-04-12 08:32:05 -0300

    removendo comentários da controller

--- src/main/java/com/example/api_manager_character_rpg/controller/CharacterController.java
+++ src/main/java/com/example/api_manager_character_rpg/controller/CharacterController.java
@@ -52,18 +52,6 @@ public class CharacterController {
         return ResponseEntity.created(uri).build();
     }
 
-//    @PutMapping("/addItem/{id}")
-//    public ResponseEntity<Character> addItem(@PathVariable Long id, @RequestBody ItemDTO itemDTO){
-//        service.addItem(id, itemDTO);
-//        return ResponseEntity.ok().build();
-//    }
-//
-//    @PutMapping("/removeItem/{id}")
-//    public ResponseEntity<Character> remove(@PathVariable Long id, @RequestBody ItemDTO itemDTO){
-//        service.removeItem(id, itemDTO);
-//        return ResponseEntity.ok().build();
-//    }
-
     @PutMapping("/{id}")
     public ResponseEntity<Void> update(@PathVariable Long id, @RequestBody UpdateNameSummonerDTO updatedNameSummonerDTO){
         service.updateNameSummoner(id, updatedNameSummonerDTO);
